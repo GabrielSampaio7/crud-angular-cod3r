f57d0eb37e47e7cfa7b2ba451fdfaf65ea4912a2
GabrielSampaio7 <gabriel.sampaio@viannasempre.com.br>
2021-08-03 18:59:09 -0300

    adicionando componente nav

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,10 +8,14 @@ import { HeaderComponent } from "./components/template/header/header.component";
 
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatToolbarModule, MatSidenavModule, MatListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
