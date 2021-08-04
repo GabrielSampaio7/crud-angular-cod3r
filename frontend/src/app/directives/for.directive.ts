253f649c151013445474ad9d34c99cd88742fef7
GabrielSampaio7 <gabriel.sampaio@viannasempre.com.br>
2021-08-04 14:18:21 -0300

    adicionando componente products-create

---
+++ frontend/src/app/directives/for.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, Input, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[myFor]'
+})
+export class ForDirective implements OnInit {
+
+  constructor() { 
+    console.log('MyFor')
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
