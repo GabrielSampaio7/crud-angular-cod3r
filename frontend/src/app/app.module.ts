8e736e2ca59653913cde0170c7a439a7762551ea
GabrielSampaio7 <gabriel.sampaio@viannasempre.com.br>
2021-08-04 15:53:18 -0300

    adicionando funcao de salvar um produto

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,10 +16,12 @@ import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { MatButtonModule } from "@angular/material/button";
 import { MatListModule } from "@angular/material/list";
 import { HomeComponent } from "./views/home/home.component";
-import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-import { RedDirective } from './directives/red.directive';
-import { ForDirective } from './directives/for.directive';
-import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductCrudComponent } from "./views/product-crud/product-crud.component";
+import { RedDirective } from "./directives/red.directive";
+import { ForDirective } from "./directives/for.directive";
+import { ProductCreateComponent } from "./components/product/product-create/product-create.component";
+
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -42,7 +44,8 @@ import { ProductCreateComponent } from './components/product/product-create/prod
     MatListModule,
     MatCardModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './../product.model';
 import { Component, OnInit } from "@angular/core";
 import { ProductService } from "./../product.service";
 import { Router } from "@angular/router";
@@ -8,12 +9,23 @@ import { Router } from "@angular/router";
   styleUrls: ["./product-create.component.css"],
 })
 export class ProductCreateComponent implements OnInit {
+
+  product: Product = {
+    name: '',
+    price: 0
+  }
+
   constructor(private productService: ProductService, private router: Router) {}
 
   ngOnInit(): void {}
 
   createProduct(): void {
-    this.productService.showMessage("Concluído com sucesso!");
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage("Concluído com sucesso!");
+      this.router.navigate(['/products'])
+    })
+
+    
   }
 
   cancel(): void {
